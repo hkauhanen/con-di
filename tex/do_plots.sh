601adb7094633fef073be473049d7c9f25dd3197
hkauhanen <nenahuak@gmail.com>
2020-10-19 13:08:39 +0200

    reaction to reviews

--- tex/do_plots.sh
+++ tex/do_plots.sh
@@ -5,7 +5,9 @@ pdflatex eigenplot2
 pdflatex eigenplot
 pdflatex bifuplot
 pdflatex sweepplot
+pdflatex evalplot
 
 cp bifuplot.pdf ../plots/Fig1.pdf
 cp sweepplot.pdf ../plots/Fig2.pdf
-cp eigenplot.pdf ../plots/Fig3.pdf
+cp evalplot.pdf ../plots/Fig3.pdf
+cp eigenplot.pdf ../plots/Fig4.pdf
